493db051f2ca9b36fe044a19769c0d6cdb963d8e
Daisuke <mhdf23888@gmail.com>
2025-07-31 15:52:46 +0800

    adding "py3/camp_202508_py3_00.py" on 31/Jul/2025

---
+++ py3/camp_202508_py3_00.py
@@ -0,0 +1,26 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/07/31 14:37:49 (UT+08:00) daisuke>
+#
+
+# importing urllib module
+import urllib.request
+
+# URL of data file
+url_data = 'https://s3b.astro.ncu.edu.tw/camp_202508/data/sample_00.data'
+
+# output file name
+file_output = 'sample_00.data'
+
+# downloading data file
+with urllib.request.urlopen (url_data) as fh_in:
+    # reading data
+    data_raw = fh_in.read ()
+    # converting data to UTF-8 string
+    data_utf8 = data_raw.decode ('utf-8')
+    
+# opening file for writing
+with open (file_output, 'w') as fh_out:
+    # writing data
+    fh_out.write (data_utf8)
